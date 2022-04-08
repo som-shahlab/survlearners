90abde2ba4d49dae2b47a9a038c334212e93f7c1
Erik Sverdrup <erik.sverdrup@gmail.com>
2022-04-07 22:04:45 -0700

    add pkg file

---
+++ survlearners/R/survlearners-package.R
@@ -0,0 +1,20 @@
+#' @description
+#' A package for...
+#'
+#' etc
+#'
+#' Some helpful links for getting started:
+#'
+#' * The R package documentation contains usage examples and method reference:
+#'
+#' * etc
+#'
+#' @examples
+#' \donttest{
+#' # The following script demonstrates...
+#' }
+#'
+#' @importFrom stats as.formula coef lm median model.matrix predict sd var weighted.mean
+#' @importFrom stats complete.cases dbeta pnorm rbinom rgamma rexp rlnorm rnorm runif rpois
+#' @keywords internal
+"_PACKAGE"
