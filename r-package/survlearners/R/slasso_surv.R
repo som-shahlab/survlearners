953da984601bbce1c811f452017e3f2ac1e90b3d
Erik Sverdrup <erik.sverdrup@gmail.com>
2022-04-08 17:42:42 -0700

    Remove caret center/scale (#61)

--- r-package/survlearners/R/slasso_surv.R
+++ r-package/survlearners/R/slasso_surv.R
@@ -47,8 +47,7 @@ slasso_surv = function(x, w, y, D, times,
   y = input$y
   D = input$D
 
-  standardization = caret::preProcess(x, method=c("center", "scale")) # get the standardization params
-  x_scl = predict(standardization, x)							                    # standardize the input
+  x_scl = scale(x, center = TRUE, scale = TRUE)
   x_scl = x_scl[,!is.na(colSums(x_scl)), drop = FALSE]
 
   nobs = nrow(x_scl)
@@ -112,8 +111,7 @@ slasso_surv = function(x, w, y, D, times,
              s_beta = s_beta_adj,
              S0_t = S0_t,
              tau_hat = tau_hat,
-             lambda_choice = lambda_choice,
-             standardization = standardization)
+             lambda_choice = lambda_choice)
 
   class(ret) <- "slasso_surv"
   ret
@@ -156,7 +154,7 @@ predict.slasso_surv <- function(object,
                                 ...) {
   if (!is.null(newx)) {
     newx = sanitize_x(newx)
-    newx_scl = predict(object$standardization, newx)
+    newx_scl = scale(newx, center = TRUE, scale = TRUE)
     newx_scl = newx_scl[,!is.na(colSums(newx_scl)), drop = FALSE]
     newx_scl_pred1 = cbind(1, newx_scl, newx_scl)
     newx_scl_pred0 = cbind(0, 0 * newx_scl, newx_scl)
