a44a2c541d5b5410ad1e2120dee5e5f6213e7413
Yizhe Xu <38216727+CrystalXuR@users.noreply.github.com>
2022-04-15 21:39:53 -0700

    update the comparison_estimators.R (#122)
    Co-authored-by: Yizhe <yizhe@DNa82112d.SUNet>

--- experiments/comparison_estimators.R
+++ experiments/comparison_estimators.R
@@ -39,58 +39,58 @@ cate_tl_grf <- function(data, data.test, t0){
 }
 
 # X-learner
-cate_xl_lasso <- function(data, data.test, t0, W.hat = 0.5){
-  fit <- surv_xl_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat)
+cate_xl_lasso <- function(data, data.test, t0, W.hat = 0.5, cen.fit = "Kaplan-Meier"){
+  fit <- surv_xl_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat, cen.fit = cen.fit)
   cate <- as.numeric(unlist(predict(fit, data.test$X)))
   cate
 }
 
-cate_xl_grf <- function(data, data.test, t0, W.hat = 0.5){
-  fit <- surv_xl_grf(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat)
+cate_xl_grf <- function(data, data.test, t0, W.hat = 0.5, cen.fit = "Kaplan-Meier"){
+  fit <- surv_xl_grf(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat, cen.fit = cen.fit)
   cate <- as.numeric(unlist(predict(fit, data.test$X)))
   cate
 }
 
-cate_xl_grf_lasso <- function(data, data.test, t0, W.hat = 0.5){
-  fit <- surv_xl_grf_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat)
+cate_xl_grf_lasso <- function(data, data.test, t0, W.hat = 0.5, cen.fit = "Kaplan-Meier"){
+  fit <- surv_xl_grf_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat, cen.fit = cen.fit)
   cate <- as.numeric(unlist(predict(fit, data.test$X)))
   cate
 }
 
 # R-learner
-cate_rl_lasso <- function(data, data.test, t0, W.hat = 0.5){
-  fit <- surv_rl_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat)
+cate_rl_lasso <- function(data, data.test, t0, W.hat = 0.5, cen.fit = "Kaplan-Meier"){
+  fit <- surv_rl_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat, cen.fit = cen.fit)
   cate <- as.numeric(unlist(predict(fit, data.test$X)))
   cate
 }
 
-cate_rl_grf <- function(data, data.test, t0, W.hat = 0.5){
-  fit <- surv_rl_grf(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat)
+cate_rl_grf <- function(data, data.test, t0, W.hat = 0.5, cen.fit = "Kaplan-Meier"){
+  fit <- surv_rl_grf(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat, cen.fit = cen.fit)
   cate <- as.numeric(unlist(predict(fit, data.test$X)))
   cate
 }
 
-cate_rl_grf_lasso <- function(data, data.test, t0, W.hat = 0.5){
-  fit <- surv_rl_grf_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat)
+cate_rl_grf_lasso <- function(data, data.test, t0, W.hat = 0.5, cen.fit = "Kaplan-Meier"){
+  fit <- surv_rl_grf_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat, cen.fit = cen.fit)
   cate <- as.numeric(unlist(predict(fit, data.test$X)))
   cate
 }
 
 # F-learner
-cate_fl_lasso <- function(data, data.test, t0, W.hat = 0.5){
-  fit <- surv_fl_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat)
+cate_fl_lasso <- function(data, data.test, t0, W.hat = 0.5, cen.fit = "Kaplan-Meier"){
+  fit <- surv_fl_lasso(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat, cen.fit = cen.fit)
   cate <- as.numeric(unlist(predict(fit, data.test$X)))
   cate
 }
 
-cate_fl_grf <- function(data, data.test, t0, W.hat = 0.5){
-  fit <- surv_fl_grf(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat)
+cate_fl_grf <- function(data, data.test, t0, W.hat = 0.5, cen.fit = "Kaplan-Meier"){
+  fit <- surv_fl_grf(data$X, data$Y, data$W, data$D, t0 = t0, W.hat = W.hat, cen.fit = cen.fit)
   cate <- as.numeric(unlist(predict(fit, data.test$X)))
   cate
 }
 
 # Causal survival forest
-cate_csf_probs <- function(data, data.test, t0, alpha = 0.05, W.hat = 0.5) {
+cate_csf_probs <- function(data, data.test, t0, alpha = 0.05, W.hat = 0.5, cen.fit = NULL) {
   fit <- grf::causal_survival_forest(X = data$X,
                                      Y = data$Y,
                                      W = data$W,
